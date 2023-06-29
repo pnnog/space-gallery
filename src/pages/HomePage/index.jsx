db4ae6616ffd25fcb4d2beab7cdc5c52fbda00e9
pnnog <paulonogueiraff@gmail.com>
2023-06-29 18:21:36 -0300

    final commit

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
-import Banner from "../../components/Banner";
-import { Footer } from "../../components/Footer";
-import { Gallery } from "../../components/Gallery";
-import Header from "../../components/Header";
-import Menu from "../../components/Menu";
-import MostPopulars from "../../components/MostPopulars";
+
+import Banner from "components/Banner";
+import Footer  from "components/Footer";
+import Gallery  from "components/Gallery";
+import Header from "components/Header";
+import Menu from "components/Menu";
+import MostPopulars from "components/MostPopulars";
 
 import styles from './homePage.module.css'
 
 export default function HomePage (){
 
-  const [mostPopulars, setMostPopulars] = useState(
+  const [mostPopulars] = useState(
     [
       {
         "id": "1",
@@ -31,19 +32,20 @@ export default function HomePage (){
     ]
   )
 
-
-
   return (
     <>
       <Header/>
-      <main>
+      <main className={styles.main}>
         <section className={styles.principal}>
           <Menu />
           <Banner />
         </section>
+
+        <div className={styles.gallery}>
+          <Gallery />
+          <MostPopulars imgs={mostPopulars} />
+        </div>
       </main>
-      <Gallery />
-      <MostPopulars imgs={mostPopulars} />
       <Footer />
     </>
   )
